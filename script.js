a42468cad47e9c51a3b9b6688db1bd4646658bb6
Rosa Turner <rmturner1220@gmail.com>
2022-05-20 20:52:40 -0400

    set criteria

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// Assignment code here
-
 // added all the symbols, number and alphabets in var chars
 var numbers = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var specialCharacters = "!@#$~%^.+-&*()";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var randomChar = function (charset) {
-  char = charset.charAt(Math.floor(Math.random()*charset.lenght));
-  return char;
-};
+function setPasswordLength() {
+  //lenght at least 8 characters ans no more than 128 characters
 
- //Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-function generatePassword() {
- window.alert("Let's create your password!");
- return password;
-}
-var setPaswordLengh = function () {
- //lenght at least 8 characters ans no more than 128 characters
-var input = window.prompt("Enter a number between 8 and 128 to set your new password");
-while(!input || input<8 || input>128) {
- window.alert("Enter a valid number.");
-}
-console.log("Password length set to:"+input);
-return input;
+  var input = window.prompt("Enter a number between 8 and 128 to set your new password");
+  
+  // used is NaN "Not-a-Number" method converts the value to a number
+  if(!input || isNaN(input) || input < 8 || input>128) {
+    window.alert("Enter a valid number.");
+  }
+  console.log("Password length set to:"+input);
+
+  return parseInt(input);
 }
 
-// Criteria password
-var setCriteria = function () {
+function yesOrNoPrompt(msg) {
+  var input = window.prompt(msg);
+
+  if(!input || ['y', 'n'].indexOf(input.toLowerCase()) < 0) {
+    window.alert("Enter yes or no.");
+  }
+
+  return input.toLowerCase() === 'y';
+}
+
+function setCriteria() {
   var criteria = {
     upperCase: false,
     lowerCase: false,
@@ -36,31 +36,73 @@ var setCriteria = function () {
     specialCharacters: false,
   };
 
-// confirm whether or not to include characters
-criteria.upperCase = yesOrNoPrompt("Should the password include uppercase? Y/N");
-criteria.lowerCase = yesOrNoPrompt("Should the password include lowercase? Y/N");
-criteria.numeric = yesOrNoPrompt("Should the password include numbers? Y/N?");
-criteria.specialCharacters = yesOrNoPrompt("Should the password include special Characters? Y/N")
+  // confirm whether or not to include characters
+  criteria.upperCase = yesOrNoPrompt("Should the password include uppercase? Y/N");
+  criteria.lowerCase = yesOrNoPrompt("Should the password include lowercase? Y/N");
+  criteria.numeric = yesOrNoPrompt("Should the password include numbers? Y/N?");
+  criteria.specialCharacters = yesOrNoPrompt("Should the password include special Characters? Y/N");
 
-console.log(criteria);
+  if (Object.values(criteria).indexOf(true) < 0) {
+    window.alert("At least one caharacter type should be selected.");
+  }
 
-return criteria;
-};
+  console.log(criteria);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  return criteria;
+}
 
 
 
 
 
+
+
+
+
+
+function generatePassword() {
+  window.alert("Let's create your password!");
+ // set pwd length
+ var pwdLength = setPasswordLength();
+
+ // set character types
+ var typesCriteria = setCriteria();
+ var characters = "";
+
+ if (typesCriteria.upperCase) {
+   characters += upperCase;
+ }
+
+ if (typesCriteria.lowerCase) {
+   characters += lowerCase;
+ }
+
+ if (typesCriteria.numeric) {
+   characters += numbers;
+ }
+
+ if (typesCriteria.specialCharacters) {
+   characters += specialCharacters;
+ }
+
+ // generate pwd
+ var password = "";
+
+ for (var i = 0; i < pwdLength; ++i) {
+   password += randomChar(characters);
+ }
+
+ return password;
+}
+
+// click btn to generate pwd
+function bindClickEvent() {
+  //Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+}
+
+bindClickEvent();
 
